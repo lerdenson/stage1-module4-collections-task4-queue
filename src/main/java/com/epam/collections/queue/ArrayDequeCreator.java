c1de33cf30e819ca6eabc6ed90835852ad4ff754
Максим <Maxim.d.morozov@gmail.com>
2023-07-12 16:56:24 +0400

    done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,26 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        ad.add(firstQueue.remove());
+        ad.add(firstQueue.remove());
+        ad.add(secondQueue.remove());
+        ad.add(secondQueue.remove());
+
+        boolean firstPriority = true;
+        while (firstQueue.size() > 0 || secondQueue.size() > 0) {
+            if (firstPriority) {
+                firstQueue.add(ad.removeLast());
+                ad.add(firstQueue.remove());
+                ad.add(firstQueue.remove());
+            } else {
+                secondQueue.add(ad.removeLast());
+                ad.add(secondQueue.remove());
+                ad.add(secondQueue.remove());
+            }
+            firstPriority = !firstPriority;
+        }
+
+        return ad;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,24 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishOrder = new ArrayList<>();
+        ArrayList<Integer> dishes = new ArrayList<>(numberOfDishes);
+        for (int i = 0; i < numberOfDishes; i++ ) {
+            dishes.add(i + 1);
+        }
+
+        int curDish = 0;
+
+        while (dishOrder.size() < numberOfDishes) {
+            curDish = ((curDish + everyDishNumberToEat - 1) % dishes.size());
+            int dish = dishes.get(curDish % dishes.size());
+            dishes.remove((Integer) dish);
+            dishOrder.add(dish);
+
+        }
+        return dishOrder;
     }
 }
